0d2302500cf422ebe54712c63f886c1827193b3d
felixx97 <abrahammfelixx@gmail.com>
2022-02-24 17:05:17 -0300

    imput observando

---
+++ src/screens/Signin/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+        justifyContent: "center",
+        alignItems: 'center',
+    },
+    input: {
+        backgroundColor: 'blue',
+        height: 50,
+        width: 200,
+        borderRadius: 15,
+    }
+});
